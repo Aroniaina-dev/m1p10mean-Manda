12ded74c872028ca523d93a342de3d6b9dae3e2d
unknown <mandaaroniaina2001@gmail.com>
2023-01-12 14:57:58 +0300

    Modification page back entier et creation login

---
+++ m1p10mean-Manda-Finaritra/src/app/modules/atelier/view/liste-atelier/liste-atelier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-atelier',
+  templateUrl: './liste-atelier.component.html',
+  styleUrls: ['./liste-atelier.component.css']
+})
+export class ListeAtelierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_LINK || 'mongodb://127.0.0.1:27017/m1p10mean-manda-finaritra-offline', { useNewUrlParser: true })
+mongoose.connect(process.env.DB_LINK, { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err));
